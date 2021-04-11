eaa1dbcd5ab00792479efa5195fc366e5f93f892
Tristan <tristan@macbook-pro-de-tristan.home>
2021-04-11 17:37:20 +0200

    begin libasm

---
+++ a64.asm
@@ -0,0 +1,19 @@
+bits 64
+
+section .data
+    message db 'Hello World !', 10
+
+section .text
+    global _start
+    _start
+        mov rax, 1                  ; equivalent de l'appel systeme write
+        mov rdi, 1                  ; declaration de l'argument, le 1 ici est le stdout
+        mov rsi, message            ; autre argument
+        mov rdx, 13 + 1             ; taille du 'message' soit 'Hello World !' + null byte
+        syscall                     ; fait appel au noyau pour executer
+        ; jusqu'ici, on a demande d'afficher le message
+
+        mov rax, 60                 ; se positionner pour faire exit
+        mov rdi, 0
+        syscall
+        ; jusque la, on a appele exit pour quitter le programme
